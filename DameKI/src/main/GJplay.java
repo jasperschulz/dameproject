9a5ab8cd02a1ae309e9f63528fc44879ac2c69e0
CryoEX <CryoEX@192.168.214.42>
2013-09-17 12:06:40 +0200

    GJplayklasse angelegt als Bearbeitungschnittstelle.

---
+++ DameKI/src/main/GJplay.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Vector;
+
+import de.dame.InvalidTurnException;
+import de.dame.Player;
+import de.dame.Turn;
+
+public class GJplay extends Player{
+
+	@Override
+	public Vector<Turn> getNextTurn(Vector<Turn> lastTurn)
+			throws InvalidTurnException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
