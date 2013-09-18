c1eec7c301d9420c1008b46d347a30cb3242562f
CryoEX <CryoEX@192.168.214.42>
2013-09-18 10:19:00 +0200

    neue Player.java

--- DameCore/src/de/dame/Player.java
+++ DameCore/src/de/dame/Player.java
@@ -4,7 +4,20 @@ import java.util.Vector;
 
 public abstract class Player {
 
+	public static final String WHITE = "white";
+
+	public static final String BLACK = "black";
+
+	private String color;
+
+	 
+
 	public abstract Vector<Turn> getNextTurn(Vector<Turn> lastTurn)
-			throws InvalidTurnException;
+
+	throws InvalidTurnException;
+
+	 
+
+	public abstract void setColor(String color);
 
 }
