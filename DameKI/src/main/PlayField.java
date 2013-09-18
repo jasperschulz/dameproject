320752dea80dd135912490a5602b5ca88ba70ce8
CryoEX <CryoEX@192.168.214.42>
2013-09-18 08:45:26 +0200

    PlayField.class für Kosolenausgabe eingefügt

---
+++ DameKI/src/main/PlayField.java
@@ -0,0 +1,21 @@
+package main;
+
+public class PlayField {
+	
+	public PlayField() {
+		init();
+	}
+	
+	private void init() {
+
+	}
+	
+	public void drawPlayField(){
+		
+	}
+	
+	public void createPlayField(){
+
+	}
+	
+}
