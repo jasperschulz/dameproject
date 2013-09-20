3e70968ff49dd14927b85ac52a4fc42ea598fbea
kidul <schulz.jasper@gmail.com>
2013-09-20 12:45:21 +0200

    playfield,field erstellt.

--- DameKI/src/units/Playfield.java
+++ DameKI/src/units/Playfield.java
@@ -1,6 +1,49 @@
 package units;
 
+import java.util.Vector;
+
 public class Playfield {
 	
+	Vector<field> fields = new Vector<field>();
+	Vector<field> displayFields = new Vector<field>();
+	
+	public Playfield() {
+		
+		addField(1,8);
+		
+		
+		
+		
+	}
+	
+	public void addField(int x, int y) {
+		
+		field newField = new field(x , y);
+		fields.add(newField);
+		
+	}
+	
+	public void generateDisplayFields() {
+		
+		int x = 1;
+		int y = 8;
+		
+		while(x != 8 && y != 1) {
+			
+			addField(x,y);
+			
+			if(x == 8){
+				
+				x = 1;
+				y--;
+				
+			} else {
+				
+				x++;
+				
+			}
+			
+		}
+	}
 
 }
--- DameKI/src/units/field.java
+++ DameKI/src/units/field.java
@@ -1,5 +1,6 @@
 package units;
 
+
 public class field {
 	
 	int x;
@@ -7,11 +8,23 @@ public class field {
 	
 	boolean occupied;
 	
+	Token token = null;
+	
 	public field (int x, int y){
 		
 		this.x = x;
 		this.y = y;
 		
+	}
+	
+	public void setToken(Token token) {
+		this.token = token;
+		this.occupied = true;
+	}
+	
+	public void removeToken() {
+		this.token = null;
+		this.occupied = false;
 	}
 
 }
