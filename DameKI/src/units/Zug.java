e2a7a000c592da178bbb96a70c9fdf9727f2c1b0
kidul <schulz.jasper@gmail.com>
2013-09-23 18:19:43 +0200

    Turn-Klasse angelegt.

---
+++ DameKI/src/units/Zug.java
@@ -0,0 +1,25 @@
+package units;
+
+import de.dame.Turn;
+
+public class Zug {
+	
+	int xLast;
+	int xNew;
+	
+	int yLast;
+	int yNew;
+	
+	public Zug (Turn turn) {
+		
+	}
+	
+	public boolean isZugValid(){
+		
+		boolean valid = false;
+		
+		return valid;
+		
+	}
+
+}
