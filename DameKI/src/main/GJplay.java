a1a64e3ceaa2a02de91c20843a4caf1749c55a22
CryoEX <CryoEX@192.168.214.42>
2013-09-24 09:20:56 +0200

    blabla

--- DameKI/src/main/GJplay.java
+++ DameKI/src/main/GJplay.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Vector;
 
+import units.Feld;
+import units.Spielfeld;
+import units.Token;
 import de.dame.InvalidTurnException;
 import de.dame.Player;
 import de.dame.Turn;
@@ -10,6 +15,13 @@ public abstract class GJplay extends Player{
 	
 	private boolean color=false;
 	private Vector<PlayField> fields;
+	private Vector<Feld> jspielfeld;
+	private Spielfeld jspielfeldobj;
+	
+	public GJplay(Spielfeld spielfeldobj){
+		this.jspielfeldobj=spielfeldobj;
+		this.jspielfeld=this.jspielfeldobj.getFields();
+	}
 
 	@Override
 	public Vector<Turn> getNextTurn(Vector<Turn> lastTurn)
@@ -35,6 +47,45 @@ public abstract class GJplay extends Player{
 	public void fillFields(int line, char column, boolean ocupied, boolean color, boolean dame){
 		PlayField field = fillField(line,column,ocupied,color,dame);
 		fields.add(field);
+	}
+	
+	public void getData() {
+		
+		Iterator<Feld> itr = this.jspielfeld.iterator();
+	
+		while(itr.hasNext()) {
+			fillFields( itr.next().getY() , transformColumn(itr.next().getX()) , itr.next().isOccupied() , transformColor(itr.next().getColor()) , itr.next().isDame());
+		}
+	}
+	
+	public char transformColumn(int x){
+		char column = ' ';
+		if(x==1)
+			column='A';
+		if(x==2)
+			column='B';
+		if(x==3)
+			column='C';
+		if(x==4)
+			column='D';
+		if(x==5)
+			column='E';
+		if(x==6)
+			column='F';
+		if(x==7)
+			column='G';
+		if(x==8)
+			column='H';
+		return column;
+	}
+	
+	public boolean transformColor(String color){
+		boolean colo = false;
+		if(color=="white")
+			colo=true;
+		if(color=="black")
+			colo=false;
+		return colo;
 	}
 
 }
