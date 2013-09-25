0098d6a4186cecc8006ed604913f4d3922d2bb0f
CryoEX <CryoEX@192.168.214.42>
2013-09-25 12:12:49 +0200

    bla

--- DameKI/src/main/DoTurn.java
+++ DameKI/src/main/DoTurn.java
@@ -3,32 +3,60 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Vector;
+
+import de.dame.Field;
+import de.dame.InvalidTurnException;
+import de.dame.Turn;
 
 public class DoTurn {
+	
+	private String sourceX;
+	private String sourceY;
+	private String destinationX;
+	private String destinationY;
+	private Turn turn = new Turn();
+	private Field source;
+	private Field destination;
+	private GJplay gjplay;
+	private Vector<Turn> lastTurn = new Vector<Turn>();
+	
+	public DoTurn(GJplay gjplay) {
+		this.gjplay=gjplay;
+	}
+	
 	
 	public void typeTurn() {
 		
 		boolean right = false;
+			
 		while(!right ){
 			
-			String zeile = setPosition();
-		
-			if (!pruefStein(zeile)) {
-			System.out.print("Du musst ein Feld wählen auf dem ein Stein von dir liegt!");
-			}
-			else {
-				right = true;
+			sourceY = setPositionY();
+			sourceX = setPositionX();
+			
+			destinationY = setDestinationY();
+			destinationX = setDestinationX();
+			
+			source = new Field(Integer.parseInt(sourceX),sourceY.charAt(0));
+			destination = new Field(Integer.parseInt(destinationX),destinationY.charAt(0));
+			
+			turn.setSource(source);
+			turn.setDestination(destination);
+			
+			lastTurn.add(turn);
+			
+			try {
+				gjplay.getNextTurn(lastTurn);
+			} catch (InvalidTurnException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
-		}
+			gjplay.setZug();
 		
-		right = false;
-		while(!right ){
-			
-			String zeile = setDestination();
-		
-			if (!pruefZug(zeile)) {
-				System.out.print("Du musst ein Feld wählen auf das du mit deinem Stein ziehen darfst!");
+			if (!pruefZug()) {
+				System.out.print("Der Zug war ungültig!");
 				}
 			else {
 				right = true;
@@ -37,16 +65,10 @@ public class DoTurn {
 		}
 	}
 	
-	private String setPosition(){
+	private String setPositionX(){
 		
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Geben sie die Zeile des Spielstein ein: ");
 		String zeile = null;
-		try {
-			zeile = console.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		System.out.print("Geben sie die Spalte des Spielstein ein: ");
 		try {
 			zeile += console.readLine();
@@ -58,16 +80,25 @@ public class DoTurn {
 		
 	}
 	
-	private String setDestination(){
+	private String setPositionY(){
 		
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Geben sie die Zielzeile für den Stein ein: ");
+		System.out.print("Geben sie die Zeile des Spielstein ein: ");
 		String zeile = null;
 		try {
 			zeile = console.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		return zeile;
+		
+	}
+	
+	private String setDestinationX(){
+		
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		String zeile = null;
 		System.out.print("Geben sie die Zielspalte für den Stein ein: ");
 		try {
 			zeile += console.readLine();
@@ -77,12 +108,22 @@ public class DoTurn {
 		
 		return zeile;
 	}
-
-	private boolean pruefZug(String zeile) {
-		return (Boolean) null;
+	
+	private String setDestinationY(){
+		
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Geben sie die Zielzeile für den Stein ein: ");
+		String zeile = null;
+		try {
+			zeile = console.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return zeile;
 	}
 
-	private boolean pruefStein(String zeile) {
+	private boolean pruefZug() {
 		return (Boolean) null;
 	}
 
