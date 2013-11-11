b47c28945645962be18dfbb364aa39a38191a7ee
kidul <schulz.jasper@gmail.com>
2013-11-11 12:01:35 +0100

    ne menge

---
+++ AI/src/de/ai/Main.java
@@ -0,0 +1,25 @@
+package de.ai;
+
+import java.util.Vector;
+
+import de.dame.InvalidTurnException;
+import de.dame.Player;
+import de.dame.Turn;
+
+
+public class Main extends Player{
+
+	@Override
+	public Vector<Turn> getNextTurn(Vector<Turn> lastTurn)
+			throws InvalidTurnException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setColor(String color) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
