2f6388933356e3b227860ac381f01b88c9310f79
kidul <js@etvnb59>
2013-11-19 12:32:53 +0100

    rules-implementierung (schritt 1)

---
+++ AI/src/de/ai/rules/Possibilities.java
@@ -0,0 +1,13 @@
+package de.ai.rules;
+
+import java.util.Vector;
+
+import de.dame.Field;
+
+public class Possibilities {
+	
+	public static Vector<Vector> getPossibleTurns(Field field) {
+		return null;
+	}
+
+}
