e5d33636c4f18296673ceae50fed1a82601e3442
kidul <js@etvnb59>
2013-11-21 14:25:13 +0100

    dddddd

--- AI/src/de/ai/rules/Possibilities.java
+++ AI/src/de/ai/rules/Possibilities.java
@@ -28,9 +28,7 @@ public class Possibilities {
 		Vector<Turn> singleTurn4 = new Vector<Turn>();
 		
 		if(possibleMoves.size() == 1 && Math.abs(possibleMoves.firstElement().getXPos() - source.getXPos()) > 1){
-			
-			//TODO: WENN DER TOKEN EINEN ANDEREN SCHLAGEN MUSS -> WIRD HIER ETWAS PASSIEREN!
-			
+						
 		} else {
 			
 			Iterator<ExtendedField> itr = possibleMoves.iterator();
